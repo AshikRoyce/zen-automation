d3025610655d5aeabfecf702532667b37d585499
Ashik <achu@Ashiks-MacBook-Pro.local>
2020-06-22 17:45:20 +0530

    index html added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Server running successfully" });
+  res.sendFile(path.join(__dirname, "../public/html/index.html"));
 });
 
 module.exports = router;
