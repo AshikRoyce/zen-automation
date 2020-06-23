149c064f44526f9bcf645736c2867933edc2dbee
ashwini <ashwini.aa2366@gmail.com>
2020-06-23 12:40:41 +0530

    fourthcommit

--- routes/automation.js
+++ routes/automation.js
@@ -13,16 +13,19 @@ router.get("/", function (req, res, next) {
 router.post("/gitclone", async (req, res) => {
   if (req.body) {
     var temp = [];
+    var temp2;
     await shell.cd("clones");
     await shell.exec(`git clone ${req.body.url} result`);
-
     const functions = require(`../clones/result/functions`);
     const expect = require(`../src/expect`);
     temp.push(expect(functions.add(2, 2)).toBe(4));
     temp.push(expect(functions.sub(5, 7)).toBe(-2));
-    shell.rm("-rf", "result");
+    shell.echo(shell.pwd());
+    temp2 = await shell.exec(`eslint result/functions.js`);
+    await shell.rm("-rf", "result");
     res.status(200).json({
-      message: temp,
+      "Testcase-Results": temp,
+      Warning: temp2,
     });
   } else {
     res.status(400).json({
@@ -59,23 +62,6 @@ router.post("/screenshot", async (req, res) => {
 
     res.status(200).json({
       message: "success",
-    });
-  } else {
-    res.status(400).json({
-      message: "bad request",
-    });
-  }
-});
-
-router.post("/eslint", async (req, res) => {
-  if (req.body) {
-    let temp;
-    await shell.exec(`git clone ${req.body.url} result`);
-    temp = await shell.exec(`eslint result/sample.js`);
-    console.log(temp);
-    shell.rm("-rf", "result");
-    res.status(200).json({
-      message: temp,
     });
   } else {
     res.status(400).json({
