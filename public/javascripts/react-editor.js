74bec8852779f26c458aca8e0572c63fab8749af
Ashik <ashikroyce@gmail.com>
2020-06-23 10:07:02 +0530

    codemirror cdn added

--- public/javascripts/react-editor.js
+++ public/javascripts/react-editor.js
@@ -10,22 +10,14 @@ const editorState = (() => {
 
 $(document).ready(function () {
   const editor = CodeMirror.fromTextArea($(".codemirror-textarea")[0], {
-    value: "function myScript(){return 100;}\n",
-    mode: "javascript",
     lineNumbers: true,
+    mode: "javascript",
   });
+
   editorState.setEditor(editor);
 });
 
-const runningjavascript = ((d) => (code) => {
-  const script = d.createElement("script");
-  script.type = "text/javascript";
-  script.async = true;
-  console.log("code::", code);
-  script.innerText = code;
-})(document);
-
-const runJs = () => {
+const runnning = () => {
   const editor = editorState.getEditor();
-  runningjavascript(editor.getValue());
+  console.log(editor.getValue());
 };
